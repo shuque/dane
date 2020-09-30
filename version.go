00f6542359e78c47135e201addf1ac545329ac78
Shumon Huque <shuque@gmail.com>
2020-09-30 08:20:26 -0400

    Bump version to 0.1.11

--- version.go
+++ version.go
@@ -65,7 +65,7 @@ package dane
 import "fmt"
 
 // Version - current version number
-var Version = VersionStruct{0, 1, 10}
+var Version = VersionStruct{0, 1, 11}
 
 // VersionStruct - version structure
 type VersionStruct struct {
